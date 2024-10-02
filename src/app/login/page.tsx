097068d77eb9b5b2ac320efd5516622b8632d2a8
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-10-02 09:30:10 +0200

    refactor: move login to separate page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/Forms/LoginForm/LoginForm";
+import Heading from "@/components/Heading/Heading";
+
+export default function page() {
+  return (
+    <div>
+      <Heading text="Добро пожаловать в сервис ФинКонтроль!" />
+      <h3 className="text-[22px]">Войдите, чтобы начать работу</h3>
+      <LoginForm />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import LoginForm from "@/components/Forms/LoginForm/LoginForm";
-import Heading from "@/components/Heading/Heading";
+import { PAGES_HEADER } from "@/data/pages";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <Heading text="Добро пожаловать в сервис ФинКонтроль!" />
-      <h3 className="text-[22px]">Войдите, чтобы начать работу</h3>
-      <LoginForm />
-    </div>
-  );
+  redirect(PAGES_HEADER[1].path);
 }
