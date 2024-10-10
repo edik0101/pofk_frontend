147d4a04d6a2623cb5df7b53cb0bad2914dee81f
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-10-10 15:50:15 +0200

    feat: add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-[calc(100vh-76px-50px)] items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-8 border-t-8 border-primary border-t-transparent"></div>
+    </div>
+  );
+}
